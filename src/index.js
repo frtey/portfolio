5d46a6f857d7bdbee74053aa47b4afe7c59f548f
Mathieu Kerherve <mathieukerherve14@gmail.com>
2021-05-12 19:58:27 +0200

    added scss support

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import barba from "@barba/core";
 import barbaCss from '@barba/css';
+import "./styles.scss";
 
 barba.use(barbaCss);
 
--- webpack.config.js
+++ webpack.config.js
@@ -12,15 +12,29 @@ module.exports = {
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        use: {
-          loader: "babel-loader",
-        },
+        loader: "babel-loader",
       },
       {
-        test: /\.scss?$/,
+        test: /\.scss$/,
         exclude: /node_modules/,
-        loader: ["css-loader", "sass-loader"],
+        use: [
+          "style-loader",
+          "css-loader", 
+          "sass-loader"
+        ]
       },
+      {
+        test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: 'fonts/'
+            }
+          }
+        ]
+      }
     ],
   },
 
