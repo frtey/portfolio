ad94aaf396669a6069387833321a69aef1743621
Mathieu Kerherve <mathieukerherve14@gmail.com>
2021-10-25 19:01:56 +0200

    small change

--- src/js/counter-index.js
+++ src/js/counter-index.js
@@ -19,7 +19,7 @@ export default function counter() {
     var promise = new Promise(function (resolve, reject) {
       window.setTimeout(function () {
         resolve({ NBPages: 151, colorPages: 47, totalPages: 198 });
-      }, 2000);
+      }, 3000);
     });
     return promise;
   }
