2c61ec591728cd5db83db6d8799aa3835e9b016e
Mathieu Kerherve <mathieukerherve14@gmail.com>
2021-10-27 19:54:06 +0200

    global front improvement

--- src/js/counter-index.js
+++ src/js/counter-index.js
@@ -3,13 +3,15 @@ import "babel-polyfill";
 export default function counter() {
   const fileSenderButton = document.getElementById("file-sender-btn");
   fileSenderButton.style.display = "none";
-  
+
   const resultArea = document.getElementById("count-result");
   resultArea.style.display = "none";
 
   document.getElementById("file-upload").onchange = () => {
     document.getElementById("custom-file-upload").style.backgroundColor =
       "#5c510b";
+    document.getElementById("custom-file-upload").style.opacity =
+      "0.8";
     fileSenderButton.style.display = "inline";
     fileSenderButton.style.marginLeft = "1vw";
     fileSenderButton.style.marginBottom = "0vh";
@@ -19,7 +21,7 @@ export default function counter() {
     var promise = new Promise(function (resolve, reject) {
       window.setTimeout(function () {
         resolve({ NBPages: 151, colorPages: 47, totalPages: 198 });
-      }, 2000);
+      }, 3000);
     });
     return promise;
   }
@@ -31,7 +33,9 @@ export default function counter() {
     const NBResultArea = document.getElementById("NbResult");
     const totalResultArea = document.getElementById("totalResult");
 
+    Loader.open();
     const result = await mockFetch();
+    Loader.close();
 
     // const formData = new FormData();
     // formData.append("pdfFile", file);
@@ -44,8 +48,10 @@ export default function counter() {
     // const result = await data.json();
 
     resultArea.style.display = "inline";
+    fileSenderButton.style.display = "none";
 
-    colorResultArea.innerHTML = "Nombre de pages couleurs : " + result.colorPages;
+    colorResultArea.innerHTML =
+      "Nombre de pages couleurs : " + result.colorPages;
     NBResultArea.innerHTML =
       "Nombre de pages noirs et blancs : " + result.NBPages;
     totalResultArea.innerHTML =
@@ -60,3 +66,61 @@ export default function counter() {
     .getElementById("file-sender-btn")
     .addEventListener("click", counterSubmitHandler);
 }
+
+// LOADER CODE
+
+const Loader = {
+  loader: null,
+  body: null,
+  html: '<span><svg width="40" height="40" version="1.1" xmlns="http://www.w3.org/2000/svg"><circle cx="20" cy="20" r="15"></svg></span>',
+  cssClass: "loader",
+  check: function () {
+    if (this.body == null) {
+      this.body = document.getElementsByTagName("body")[0];
+    }
+  },
+  open: function () {
+    this.check();
+    if (!this.isOpen()) {
+      this.loader = document.createElement("div");
+      this.loader.setAttribute("id", "loader");
+      this.loader.classList.add("loader_website");
+      this.loader.innerHTML = this.html;
+      this.body.append(this.loader);
+      this.body.classList.add(this.cssClass);
+    }
+    return this;
+  },
+  close: function () {
+    this.check();
+    if (this.isOpen()) {
+      this.body.classList.remove(this.cssClass);
+      this.loader.remove();
+    }
+    return this;
+  },
+  isOpen: function () {
+    this.check();
+    return this.body.classList.contains(this.cssClass);
+  },
+  ifOpened: function (callback, close) {
+    this.check();
+    if (this.isOpen()) {
+      if (!!close) this.close();
+      if (typeof callback === "function") {
+        callback();
+      }
+    }
+    return this;
+  },
+  ifClosed: function (callback, open) {
+    this.check();
+    if (!this.isOpen()) {
+      if (!!open) this.open();
+      if (typeof callback === "function") {
+        callback();
+      }
+    }
+    return this;
+  },
+};
